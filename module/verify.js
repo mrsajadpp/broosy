fc7e4a77af0ab3e58d56bd7298c970ae7389a49b
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-08-06 21:18:24 +0530

    update verify.js

--- module/verify.js
+++ module/verify.js
@@ -3,21 +3,15 @@ function verify(client, message) {
   if (msg.guild.roles.cache.find(r => r.name === "familiar") && msg.guild.roles.cache.find(r => r.name === "stranger")) {
     if (msg.member.roles.cache.find(r => r.name === "familiar")) {
       msg.author.send('You already verified.').catch(console.error);
-      client = null;
-      message = null;
     } else {
      let role = msg.guild.roles.cache.find(r => r.name === "familiar");
      let rm = msg.guild.roles.cache.find(r => r.name === "stranger");
       msg.member.roles.add(role).catch(console.error);
       msg.member.roles.remove(rm).catch(console.error);
       msg.author.send('<@'+msg.author.id+'>, Succesfully verified.').catch(console.error);
-      client = null;
-      message = null;
     }
   } else {
     msg.channel.send("Oops!. can't find familiar and stranger roles.").catch(console.error);
-    client = null;
-    message = null;
   }
 }
 module.exports = verify;
