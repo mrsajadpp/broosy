9667c70b8bf0f7200264cd9861c713cf79bcefe3
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-08-06 17:57:10 +0530

    update verify.js

--- module/verify.js
+++ module/verify.js
@@ -1,7 +1,7 @@
 function verify(client, message, author) {
-  let msg = message;
-  let auth = author;
-  let role = msg.guild.roles.cache.find(r => r.name === "familiar");
+  var msg = message;
+  var auth = author;
+  var role = msg.guild.roles.cache.find(r => r.name === "familiar");
   msg.member.roles.add(role).catch(console.error);
   let rm = msg.guild.roles.cache.find(r => r.name === "stranger");
   msg.member.roles.remove(rm).catch(console.error);
