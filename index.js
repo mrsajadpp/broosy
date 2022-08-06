91e090c16544b57a29dcbe4cebd83afb471477db
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-08-06 16:37:36 +0530

    update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const discord = require('discord.js');
 const fs = require('fs');
 const path = require('path');
 let client = new discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-let port = process.env.PORT;
+let port = 3000;
 let prefix = ':';
 let { listen, ran } = require('./server.js');
 client.on('ready', () => {
@@ -22,14 +22,13 @@ client.on('messageCreate', message => {
   client.user.setActivity("captcha", {
         type: "WATCHING"
   });
-  
   if (message.content.startsWith(prefix)) {
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const commandName = args.shift();
     const command = client.commands.get(commandName);
     if(!command) return message.reply({ content: "Check your spelling before sending."});
-    command.run(client, message, args, ran);
+    command.run(client, message, args, ran, port);
   }
 });
 listen(port,client);
-client.login(process.env.TOKEN);
+client.login('OTk2NjUxNDkzMDY1MjMyNDA1.GaaDoy.-W8TwU3Yv9CBxc28ly4gIcMv77zTrlJVKQqMoI');
