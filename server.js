270afa0b672789de0cd7fb5b9a956d5d8177ab64
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-08-06 12:15:51 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+let app = express();
+let verify = require('./module/verify.js');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+function listen(port, clnt, msg) {
+  app.listen(port, () => {
+    client = clnt;
+    message = msg;
+    console.log('Server is running on port : '+port);
+  });
+}
+function ran(client, message) {
+  app.get(`/${message.author.id}`, (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+  });
+  app.post('/captcha', (req, res) => {
+    if (req.body.o == req.body.c) {
+      verify(client, message);
+      res.sendFile(path.join(__dirname, 'views/final.html'));
+    } else {
+      res.sendFile(path.join(__dirname, 'views/error.html'));
+    }
+  });
+}
+module.exports = {
+  listen,
+  ran
+};
