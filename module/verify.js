5ef5cef51784764ef61558bfb894af60817761f7
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-08-06 20:59:13 +0530

    update verify.js

--- module/verify.js
+++ module/verify.js
@@ -1,20 +1,23 @@
-function verify(client, message, author) {
-  var msg = message;
-  var auth = author;
-  var role;
-  var rm;
+function verify(client, message) {
+  let msg = message;
   if (msg.guild.roles.cache.find(r => r.name === "familiar") && msg.guild.roles.cache.find(r => r.name === "stranger")) {
     if (msg.member.roles.cache.find(r => r.name === "familiar")) {
       msg.author.send('You already verified.');
+      client = null;
+      message = null;
     } else {
-      role = msg.guild.roles.cache.find(r => r.name === "familiar");
-      rm = msg.guild.roles.cache.find(r => r.name === "stranger");
+     let role = msg.guild.roles.cache.find(r => r.name === "familiar");
+     let rm = msg.guild.roles.cache.find(r => r.name === "stranger");
       msg.member.roles.add(role).catch(console.error);
       msg.member.roles.remove(rm).catch(console.error);
       msg.author.send('<@'+msg.author.id+'>, Succesfully verified.');
+      client = null;
+      message = null;
     }
   } else {
     msg.channel.send("Oops!. can't find familiar and stranger roles.");
+    client = null;
+    message = null;
   }
 }
 module.exports = verify;
