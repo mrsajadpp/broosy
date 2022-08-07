6ad920af5184fe6230278e48e6bf4b402f19f758
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-08-07 13:34:48 +0530

    update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -1,5 +1,5 @@
 module.exports.run = (client, message, args, ran, port) => {
   message.author.send(`<@${message.author.id}>, Are you a human?. verify using this link. Website: https://broosy.sajadpp.repl.co/${message.author.id}`).catch(console.error);
-  message.channel.send('<@'+message.author.id+'>, Checkout your dm.').catch(console.error);
+  message.channel.send('<@'+message.author.id+'>, 📩 Checkout your dm.').catch(console.error);
   ran(message);
 }
